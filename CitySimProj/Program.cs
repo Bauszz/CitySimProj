b809c85b9cfe2713a8eef7a260985db33f25179c
Petrik Diák <diak@petrik.hu>
2026-03-03 08:56:30 +0100

    MŰKÖDIK!!!!

---
+++ CitySimProj/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CitySimProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
